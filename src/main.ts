7d60024a9dbef010e94a1701cb45c4e317e0971c
Bartu Gençcan <bartugenccandev@gmail.com>
2023-07-02 17:14:53 +0300

    feat: Cookie Session setup done at main

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,14 @@ import { AppModule } from './app.module';
 // Validation Pipe
 import { ValidationPipe } from '@nestjs/common';
 
+// Cookie Session
+const cookieSession = require('cookie-session');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: ['asdf'],
+  }))
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, // remove properties that are not in the DTO
   }));
